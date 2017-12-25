ca2972874274f995278d9dabb27c0b28e3e684bb
Connor Hamm <connorjhamm@gmail.com>
2017-12-25 08:25:52 -0800

    added only audio option

--- youtube.py
+++ youtube.py
@@ -7,6 +7,22 @@ var = raw_input("Enter URL: ")
 print "\nDownloading to \"media\" folder"
 
 os.chdir("/home/connor/Documents/media")
-ydl_opts = {}
+
+x = raw_input("Select Format: (1) - audio (2) - audio+video\n")
+
+if x == "1":
+	print "audio selected\n"
+	ydl_opts = {
+		'format': 'bestaudio/best',
+		'postprocessors': [{
+		'key': 'FFmpegExtractAudio',
+		'preferredcodec': 'mp3',
+		'preferredquality': '192',
+	}],
+}
+else:
+	print "audio + video selected\n"
+	ydl_opts = {}
+
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([var])
